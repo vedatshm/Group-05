e986e423d5940bc7ea56db373a2b7b7a7ac4fc22
Vedat Selimi <vedat.selimi01@gmail.com>
2024-06-20 20:22:22 +0200

    upload first work

--- VIIC Group 5-Copy/.local/install/CodedWorkflow.cs
+++ VIIC Group 5-Copy/.local/install/CodedWorkflow.cs
@@ -16,7 +16,7 @@ namespace VIICGroup5Copy
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Core.Activities.API.ISystemService)};
+            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
@@ -29,6 +29,15 @@ namespace VIICGroup5Copy.ObjectRepository
 {
     public static class Descriptors
     {
+        public static class Edge__HM_KI
+        {
+            public static _Implementation._Edge__HM_KI.__Edge__HM_KI Edge__HM_KI { get; private set; } = new _Implementation._Edge__HM_KI.__Edge__HM_KI();
+        }
+
+        public static class Edge__Instagram_Hashtag_Generator___SISTRIX
+        {
+            public static _Implementation._Edge__Instagram_Hashtag_Generator___SISTRIX.__Edge__Instagram_Hashtag_Generator___SISTRIX Edge__Instagram_Hashtag_Generator___SISTRIX { get; private set; } = new _Implementation._Edge__Instagram_Hashtag_Generator___SISTRIX.__Edge__Instagram_Hashtag_Generator___SISTRIX();
+        }
     }
 }
 
@@ -54,5 +63,87 @@ namespace VIICGroup5Copy._Implementation
         public IElementDescriptor ParentElement { get; set; }
 
         public IElementDescriptor Element { get; set; }
+    }
+
+    namespace _Edge__HM_KI._Edge__HM_KI
+    {
+        public class __System_Prompt_ : IElementDescriptor
+        {
+            private readonly IScreenDescriptor _screenDescriptor;
+            private readonly IElementDescriptor _parentElementDescriptor;
+            private readonly IElementDescriptorDefinition _elementDescriptor;
+            public IElementDescriptorDefinition GetDefinition()
+            {
+                return _elementDescriptor;
+            }
+
+            public __System_Prompt_(IScreenDescriptor screenDescriptor, IElementDescriptor parentElementDescriptor)
+            {
+                _screenDescriptor = screenDescriptor;
+                _parentElementDescriptor = parentElementDescriptor;
+                _elementDescriptor = new ElementDescriptorDefinition{Reference = "PxXJJuDVtEyKTjAMlPTo5w/1kG8RIDUP0yCOQ4_I_3bHw", DisplayName = "System Prompt:", Element = this, ParentElement = _parentElementDescriptor, Screen = screenDescriptor};
+            }
+        }
+    }
+
+    namespace _Edge__HM_KI
+    {
+        public class __Edge__HM_KI : IScreenDescriptor
+        {
+            public IScreenDescriptorDefinition GetDefinition()
+            {
+                return _screenDescriptor;
+            }
+
+            private readonly ScreenDescriptorDefinition _screenDescriptor;
+            public __Edge__HM_KI()
+            {
+                _screenDescriptor = new ScreenDescriptorDefinition{Reference = "PxXJJuDVtEyKTjAMlPTo5w/vM2HtSrgmUmI2vL_sW1Z5g", DisplayName = "Edge: HM-KI", Screen = this};
+                System_Prompt_ = new _Implementation._Edge__HM_KI._Edge__HM_KI.__System_Prompt_(this, null);
+            }
+
+            public _Implementation._Edge__HM_KI._Edge__HM_KI.__System_Prompt_ System_Prompt_ { get; private set; }
+        }
+    }
+
+    namespace _Edge__Instagram_Hashtag_Generator___SISTRIX._Edge__Instagram_Hashtag_Generator___SISTRIX
+    {
+        public class __Instagram_Hashtag : IElementDescriptor
+        {
+            private readonly IScreenDescriptor _screenDescriptor;
+            private readonly IElementDescriptor _parentElementDescriptor;
+            private readonly IElementDescriptorDefinition _elementDescriptor;
+            public IElementDescriptorDefinition GetDefinition()
+            {
+                return _elementDescriptor;
+            }
+
+            public __Instagram_Hashtag(IScreenDescriptor screenDescriptor, IElementDescriptor parentElementDescriptor)
+            {
+                _screenDescriptor = screenDescriptor;
+                _parentElementDescriptor = parentElementDescriptor;
+                _elementDescriptor = new ElementDescriptorDefinition{Reference = "PxXJJuDVtEyKTjAMlPTo5w/gH52rrzfRUWf4j0XbwSgrg", DisplayName = "Instagram Hashtag", Element = this, ParentElement = _parentElementDescriptor, Screen = screenDescriptor};
+            }
+        }
+    }
+
+    namespace _Edge__Instagram_Hashtag_Generator___SISTRIX
+    {
+        public class __Edge__Instagram_Hashtag_Generator___SISTRIX : IScreenDescriptor
+        {
+            public IScreenDescriptorDefinition GetDefinition()
+            {
+                return _screenDescriptor;
+            }
+
+            private readonly ScreenDescriptorDefinition _screenDescriptor;
+            public __Edge__Instagram_Hashtag_Generator___SISTRIX()
+            {
+                _screenDescriptor = new ScreenDescriptorDefinition{Reference = "PxXJJuDVtEyKTjAMlPTo5w/03IkPjn-0ESlWvOGZ3X_qg", DisplayName = "Edge: Instagram Hashtag Generator - SISTRIX", Screen = this};
+                Instagram_Hashtag = new _Implementation._Edge__Instagram_Hashtag_Generator___SISTRIX._Edge__Instagram_Hashtag_Generator___SISTRIX.__Instagram_Hashtag(this, null);
+            }
+
+            public _Implementation._Edge__Instagram_Hashtag_Generator___SISTRIX._Edge__Instagram_Hashtag_Generator___SISTRIX.__Instagram_Hashtag Instagram_Hashtag { get; private set; }
+        }
     }
 }
