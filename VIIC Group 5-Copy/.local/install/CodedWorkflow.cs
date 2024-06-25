238e4e7d13309edecd44b6ab25b3f9d704a2d65b
Vedat Selimi <vedat.selimi01@gmail.com>
2024-06-25 02:58:02 +0200

    Social media integrated instagram and twitter

--- VIIC Group 5-Copy/.local/install/CodedWorkflow.cs
+++ VIIC Group 5-Copy/.local/install/CodedWorkflow.cs
@@ -16,7 +16,7 @@ namespace VIICGroup5Copy
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Core.Activities.API.ISystemService)};
+            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
