0997f2c46a38fe42ccde8acd3c758cb0f72c0f7c
Vedat Selimi <vedat.selimi01@gmail.com>
2024-07-08 00:23:27 +0200

    final projekt

--- VIIC Group 5-Copy/.local/install/CodedWorkflow.cs
+++ VIIC Group 5-Copy/.local/install/CodedWorkflow.cs
@@ -16,7 +16,7 @@ namespace VIICGroup5Copy
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
+            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Core.Activities.API.ISystemService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
